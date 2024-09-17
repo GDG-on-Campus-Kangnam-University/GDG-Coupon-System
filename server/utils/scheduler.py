9c04fb38ed93a825e56896fe7881b9359421e289
hin6150 <hin6150@gmail.com>
2024-09-17 22:14:10 +0900

    feat: 스케줄러 작동 시간 수정

--- server/utils/scheduler.py
+++ server/utils/scheduler.py
@@ -13,18 +13,29 @@ def fetch_and_update_google_sheets_data():
         db.close()  # 세션 종료
     return updates_apply, updates_finance
 
+def should_skip_execution():
+    now = datetime.now()
+    # 새벽 1시부터 아침 9시까지는 실행하지 않음
+    if 1 <= now.hour < 9:
+        return True
+    return False
+
 def schedule_next_run():
     now = datetime.now()
-    next_minute = 30 if now.minute < 30 else 0
-    # next_hour = now.hour if now.minute < 30 else (now.hour + 1) % 24
-    next_run_time = now.replace(minute=next_minute, second=0, microsecond=0)
-    if next_minute == 0 and now.minute >= 30:
-        next_run_time += timedelta(hours=1)
-
-    delay = (next_run_time - now).total_seconds()
-    time.sleep(delay)
-    fetch_and_update_google_sheets_data()
-    schedule.every(30).minutes.do(fetch_and_update_google_sheets_data)
+    # 다음 실행 시간을 한 시간 단위로 설정
+    next_run_time = now.replace(minute=0, second=0, microsecond=0) + timedelta(hours=1)
+    
+    # 실행을 건너뛸 시간대인지 확인
+    if should_skip_execution():
+        print("현재는 실행 시간대가 아닙니다. 다음 실행 시간을 대기합니다.")
+        delay = (next_run_time - now).total_seconds()
+        time.sleep(delay)
+        schedule_next_run()
+    else:
+        delay = (next_run_time - now).total_seconds()
+        time.sleep(delay)
+        fetch_and_update_google_sheets_data()
+        schedule.every().hour.do(fetch_and_update_google_sheets_data)
 
 def run_scheduler():
     schedule_next_run()
